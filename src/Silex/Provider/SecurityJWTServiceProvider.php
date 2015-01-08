95f0a5aef9999645062dc2d24cdf131892928aa1
cnam <cnam812@gmail.com>
2015-01-08 17:07:53 +0300

    правки по секретному ключу

--- src/Silex/Provider/SecurityJWTServiceProvider.php
+++ src/Silex/Provider/SecurityJWTServiceProvider.php
@@ -33,7 +33,7 @@ class SecurityJWTServiceProvider implements ServiceProviderInterface
 
         $app['security.authentication_listener.factory.jwt'] = $app->protect(function ($name, $options) use ($app) {
             $app['security.authentication_listener.'.$name.'.jwt'] = function() use ($app) {
-                return new JWTListener($app['security'], $app['security.authentication_manager'], $app['security.jwt']['secret_token']);
+                return new JWTListener($app['security'], $app['security.authentication_manager'], $app['security.jwt']['secret_key']);
             };
 
             $app['security.authentication_provider.' . $name . '.jwt'] = function() use ($app) {
