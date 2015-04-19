06ca3dc93bde433bf3b4e7daf9792a64f325d977
cnam <cnam812@gmail.com>
2015-04-19 18:30:11 +0300

    anonymouse fixes

---
+++ src/Silex/Component/Security/Http/EntryPoint/JwtAuthenticationEntryPoint.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cnam
+ * Date: 19/04/15
+ * Time: 17:23
+ */
+
+namespace Silex\Component\Security\Http\EntryPoint;
+
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\EntryPoint\AuthenticationEntryPointInterface;
+
+class JwtAuthenticationEntryPoint implements AuthenticationEntryPointInterface
+{
+
+    /**
+     * Starts the authentication scheme.
+     *
+     * @param Request                 $request       The request that resulted in an AuthenticationException
+     * @param AuthenticationException $authException The exception that started the authentication process
+     *
+     * @return Response
+     */
+    public function start(Request $request, AuthenticationException $authException = null)
+    {
+        return new JsonResponse(['message' => $authException->getMessage()], Response::HTTP_UNAUTHORIZED);
+    }
+}
